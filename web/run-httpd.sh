9f31efab5ceb7c3797d1e66fdd049b36c7ba1ce0
Bohdan Bashynskyi <generydey@localhost.localdomain>
2018-11-28 14:57:16 +0100

    Last working version

--- web/run-httpd.sh
+++ web/run-httpd.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-svn export https://github.com/bbashinskiy/kxtp/trunk/web/kxtp /var/www/html --force &&\
+svn export https://github.com/bbashinskiy/kxtp/trunk/web/kxtp /var/www/kxtp.kpi.ua/htdocs --force &&\
 
-chmod 777 /var/www/html/cache && \
-chown -Rf apache:apache /var/www/html/*
+chown -Rf apache:apache /var/www/kxtp.kpi.ua/htdocs/*
 
 exec /usr/sbin/apachectl -DFOREGROUND
 
